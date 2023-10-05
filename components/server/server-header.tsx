cc914b320abbe58d1d7ed6de136d99bb40703336
Ryan Agung Samudra <ryansamudra67@gmail.com>
2023-10-06 01:03:41 +0700

    feat: chat header and mobile toogle button

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -37,7 +37,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         <DropdownMenuTrigger className="focus:outline-none" asChild>
           <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10  dark:hover:bg-zinc-700/50 transition">
             {server?.name}
-            <ChevronDown className="h-5 w-5 ml-auto" size={16} />
+            <ChevronDown
+              className="h-5 w-5 ml-auto hidden md:block"
+              size={16}
+            />
           </button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
